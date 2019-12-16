c970afe0e9a4716f9aa79048b9a21e01bc14a6ef
ravikiran <rvkrnunix@gmail.com>
2019-12-16 19:31:55 +0530

    eduapp integrated without test cases

--- src/test/java/com/intebral/Tests/IntegrationTest.java
+++ src/test/java/com/intebral/Tests/IntegrationTest.java
@@ -1,10 +1,54 @@
 package com.intebral.Tests;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class IntegrationTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 WebDriver driver;
+     String baseURL, nodeURL;
 
-	}
+	@BeforeTest
+     public void setUp() throws MalformedURLException {
+		System.out.println("getClass "+this.getClass());  
+		System.out.println("System.getProperty(testSuite) "+System.getProperty("testSuite")); 
+		System.out.println("System.getProperty(testSuite) "+System.getProperty("surefire.suiteXmlFiles")); 
+
+		 baseURL = "http://34.69.19.204:9999/MyDevPipeLine/";
+         nodeURL = "http://35.200.150.252:4444/wd/hub";
+         DesiredCapabilities capability = DesiredCapabilities.chrome();
+         capability.setBrowserName("chrome");
+         capability.setPlatform(Platform.LINUX);
+         driver = new RemoteWebDriver(new URL(nodeURL), capability);
+         
+     }
+
+     @AfterTest
+     public void afterTest() {
+         driver.quit();
+     }
+     @Test
+     public void sampleTest() {
+         driver.get(baseURL);
+         System.out.println("driver.getTitle() "+driver.getTitle()); 
+         System.out.println("driver.getTitle().contains(\"DevOps Registration Form\") "+driver.getTitle().contains("DevOps Registration Form")); 
+         if (driver.getTitle().contains("DevOps Registration Form")) {
+             AssertJUnit.assertTrue( "DevOps Registration Form", true);
+         } else {
+             AssertJUnit.assertTrue("Failed: DevOps Registration Form",false );
+         }
+
+     }
+
+
 
 }
